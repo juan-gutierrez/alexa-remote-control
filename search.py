359ba0fefde24f0a43e5f26e0be687ba36dec088
Juan Gutierrez <juan.gutierrez@nxp.com>
2020-07-31 16:11:49 -0700

    Alexa working with Lavadora
    Signed-off-by: Juan Gutierrez <juan.gutierrez@nxp.com>

---
+++ search.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import time
+
+getState = False
+getJobState = False
+currentJobState = "none"
+newJobState = "none"
+
+while True:
+
+    os.system("./test.py > mytest.txt")
+    
+
+    with open("mytest.txt") as openfile:
+        for line in openfile:
+            for part in line.split():
+                if (getState):
+                    print(part)
+                    currentState = part
+                    getState = False
+                if (getJobState):
+                    print(part)
+                    newJobState = part
+                    getJobState = False
+                if "'machineState'" in part:
+                    getState = True
+                if "'washerJobState'" in part:
+                    getJobState = True
+            
+
+    print("Current states is ", currentState)
+
+    if (re.search("run", currentState)):
+        print("Lavadora is running")
+        if (re.search("weightsensing", newJobState)):
+           print("La lavadora esta calentando motores")
+           mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"La lavadora esta calentando motores\""
+           mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"La lavadora esta calentando motores\""
+        if (re.search("wash", newJobState)):
+           print("La lavadora comenzo a lavar, chaca chaca")
+           mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"La lavadora comenzo a lavar, chaca chaca\""
+           mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"La lavadora comenzo a lavar, chaca chaca\""
+        if (re.search("rinse", newJobState)):
+           print("La lavadora esta enjuagando con Suavitel, pa dejar suavecita la ropita del Gato")
+           mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"La lavadora esta enjuagando con Suavitel, pa dejar suavecita la ropita del Gato\""
+           mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"La lavadora esta enjuagando con Suavitel, pa dejar suavecita la ropita del Gato\""
+        if (re.search("spin", newJobState)):
+           print("La lavadora ta exprimiendo, gira que gira")
+           mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"La lavadora ta exprimiendo, gira que gira\""
+           mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"La lavadora ta exprimiendo, gira que gira\""
+        if (re.search("drying", newJobState)):
+           print("La lavadora ta secando, que calorsh")
+           mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"La lavadora ta secando, que calorsh\""
+           mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"La lavadora ta secando, que calorsh\""
+        if (re.search("finish", newJobState)):
+            print("Ya esta la ropita")
+            mystring = "./alexa_remote_control.sh -d \"Comedor\" -e speak:" + "\"Ya esta la ropita\""
+            mystring2 = "./alexa_remote_control.sh -d \"Dormitorio\" -e speak:" + "\"Ya esta la ropita\""
+
+        if (newJobState != currentJobState):
+            currentJobState = newJobState;
+            os.system(mystring)
+            time.sleep(2)
+            os.system(mystring2)
+
+    time.sleep(5)
